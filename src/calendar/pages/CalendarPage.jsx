eb997444e58bfd6743edbb0b044f36855475a25a
xansiety <ferando543@outlook.com>
2022-11-21 14:27:45 -0600

    Diseño: Instalacion y configuracion inicial de @react-big-calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,50 @@
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import {addHours, parse, startOfWeek, getDay, format} from 'date-fns'
+import enUS from 'date-fns/locale/en-US'
+
 import { NavBar } from "../"
 
- 
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+
+
+const events = [
+  {
+    title: 'My event',
+    notes: 'Some notes about this event',
+    start: new Date(),
+    end:  addHours(new Date(), 2),
+    bgColor: '#f0f0f0',
+    user:{
+      _id: '123',
+      name: 'xansiety'
+    }
+  }
+]
+
+
 export const CalendarPage = () => {
   return (
     <>
       <NavBar />
+      <Calendar
+      localizer={localizer}
+      events={events}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 'calc( 100vh - 80px)' }}
+    />
     </>
   )
 }
