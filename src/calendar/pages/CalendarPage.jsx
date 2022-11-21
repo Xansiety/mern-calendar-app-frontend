7e5e8a60fd1a7143a72af8a9807f39528068fa0d
xansiety <ferando543@outlook.com>
2022-11-21 14:46:03 -0600

    Calendario -> Configuraciones adicionales en calendario, mensajes en español

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,50 +1,52 @@
-import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
-import {addHours, parse, startOfWeek, getDay, format} from 'date-fns'
-import enUS from 'date-fns/locale/en-US'
-
-import { NavBar } from "../"
-
-const locales = {
-  'en-US': enUS,
-}
-
-const localizer = dateFnsLocalizer({
-  format,
-  parse,
-  startOfWeek,
-  getDay,
-  locales,
-})
-
-
+import { Calendar } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { addHours } from "date-fns";
+import { NavBar } from "../";
+import { localizer, getMessagesCalendarES } from "../../helpers";
 
 const events = [
   {
-    title: 'My event',
-    notes: 'Some notes about this event',
+    title: "My event",
+    notes: "Some notes about this event",
     start: new Date(),
-    end:  addHours(new Date(), 2),
-    bgColor: '#f0f0f0',
-    user:{
-      _id: '123',
-      name: 'xansiety'
-    }
-  }
-]
-
+    end: addHours(new Date(), 2),
+    bgColor: "#f0f0f0",
+    user: {
+      _id: "123",
+      name: "xansiety",
+    },
+  },
+];
 
 export const CalendarPage = () => {
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    console.log({ event, start, end, isSelected });
+
+    const style = {
+      backgroundColor: "#367cf7",
+      borderRadius: "0px",
+      opacity: 0.8,
+      color: "white",
+    };
+
+    return {
+      style,
+    };
+  };
+
   return (
     <>
       <NavBar />
       <Calendar
-      localizer={localizer}
-      events={events}
-      startAccessor="start"
-      endAccessor="end"
-      style={{ height: 'calc( 100vh - 80px)' }}
-    />
+        culture="es"
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc( 100vh - 80px)" }}
+        messages={getMessagesCalendarES()}
+        eventPropGetter={eventStyleGetter}
+      />
     </>
-  )
-}
+  );
+};
